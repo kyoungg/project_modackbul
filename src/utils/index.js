b38dcc79af75f8bda5e35e061f06635ae5353f77
Rki0 <pky00823@gmail.com>
2023-04-19 23:42:20 +0900

    feat(utils): 공통 기능 함수 추가
    이메일, 이름, 휴대전화 번호 검증 함수를 추가했습니다.
    이메일은 abc@abc.com 의 형태,
    이름은 한글로 된 2~4글자(모음, 자음만 사용하는 건 안됨),
    휴대전화 번호는 000-0000-0000 의 형태여야 true를 반환합니다.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,49 @@
-// 문자열 파싱 등 기능 함수
+/**
+ * 이메일 입력값을 정규식에 따라 검증하는 함수
+ * @param {String} email 이메일 문자열
+ * @returns 이메일 형식에 맞으면 true, 아니면 false를 반환
+ */
+export function checkEmail(email) {
+  const regex = /^[a-z0-9\.\-_]+@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
+
+  if (!regex.test(email)) {
+    alert("이메일 형식이 올바르지 않습니다.");
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * 이름 입력값을 정규식에 따라 검증하는 함수
+ * @param {String} name 이름 문자열
+ * @returns 이름 형식에 맞으면 true, 아니면 false 반환
+ */
+export function checkName(name) {
+  const regex = /^[가-힣]{2,4}$/;
+
+  if (!regex.test(name)) {
+    alert("이름을 다시 확인해주세요. 한글로 2 ~ 4자 까지 입력 가능합니다.");
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * 휴대전화 번호 입력값을 정규식에 따라 검증하는 함수
+ * @param {String} phoneNumber 휴대전화 번호 문자열
+ * @returns 휴대전화 번호 형식에 맞으면 true, 아니면 false 반환
+ */
+export function checkPhoneNumber(phoneNumber) {
+  const regex = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/;
+
+  if (!regex.test(phoneNumber)) {
+    alert(
+      "휴대전화 번호를 다시 확인해주세요. 000-0000-0000 형식으로 입력해주세요."
+    );
+    return false;
+  }
+
+  return true;
+}
