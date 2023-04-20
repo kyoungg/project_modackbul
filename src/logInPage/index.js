6817b6c3f2ce5aed37f4e81cddf26619d3e34a18
kyounghye <kyoung00828@gmail.com>
2023-04-20 12:49:12 +0900

    Feat: 로그인 페이지 구현 이메일 검증 추가 받아온 데이터 저장, 비회원 주문조회 버튼 기능 추가

--- src/logInPage/index.js
+++ src/logInPage/index.js
@@ -1,3 +1,5 @@
+import { checkEmail } from "../utils/index.js"
+
 const emailInput = document.querySelector("#emailInput");
 const passwordInput = document.querySelector("#passwordInput");
 const loginBtn = document.querySelector("#loginBtn");
@@ -23,7 +25,9 @@ async function loginSubmit(e) {
     // 입력 여부 확인
     if (!email) {
       return alert("이메일을 입력해 주세요.")
-    };
+    } else {
+      checkEmail()
+    }
     
     if (!password) {
       return alert("비밀번호를 입력해 주세요.")
@@ -32,7 +36,7 @@ async function loginSubmit(e) {
     // JSON 만들기
     const dataJson = JSON.stringify(data)
     
-    const apiUrl = `#`
+    const apiUrl = `/api/users/login` //수정 필요할지도
   
     const res = await fetch(apiUrl, {
       method: 'POST',
@@ -42,15 +46,16 @@ async function loginSubmit(e) {
       body: dataJson,
     });
     
-    // // 물론 실제로는 로그인의 경우 201보다는 200이 더 맞긴 하지만, 실습 환경 상 201로 해야 합니다.
     // if (res.status === 200) {
     //   alert("로그인에 성공하였습니다!")
+    //   window.location.href = '/landingPage';
     // } else {
     //   alert("로그인에 실패하였습니다...")
+    //   window.location.href = '/loginPage';
     // }
   }
 
-  //취소버튼 이벤트
+  //비회원 주문조회 이벤트
   nonmemberBtn.addEventListener("click", ()=>{
     window.location.href = '/adminMypage';
   });
