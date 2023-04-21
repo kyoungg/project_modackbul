3108c6e41a70b7c67415fe29785d3b881d2bcb9d
kyounghye <kyoung00828@gmail.com>
2023-04-21 19:27:42 +0900

    feat:관리자 상품수정페이지 구현 관리자가 상품을 상세를 수정할 수 있는 페이지입니다. 이미지. 카테고리와 관련된 내용은 아직 구현하지 못했습니다. 페이지에 접근했을 때. 이미 등록되어 있는 상품의 preview가 작동하지 않고 있습니다..

---
+++ src/adminMyPage/form.js
@@ -0,0 +1,99 @@
+export function productForm() {
+    const field =
+    `<div class="addproduct-form-container">
+        <form id="productForm">
+            <h3>상품 수정 페이지</h3>
+            <div>
+                <label for="nameInput">상품이름</label>
+                <div>
+                    <input
+                    type="text"
+                    id="nameInput"
+                    required
+                    >
+                </div>
+            </div>
+            <div>
+                <label for="priceInput">상품가격</label>
+                <div>
+                    <input
+                    type="number"
+                    id="priceInput"
+                    required
+                    >
+                </div>
+            </div>
+            <div>
+                <label for="summaryInput">상품소개</label>
+                <div>
+                    <input
+                    type="text"
+                    id="summaryInput"
+                    required
+                    >
+                </div>
+            </div>
+            <div>
+                <label for="companyInput">제조회사</label>
+                <div>
+                    <input
+                    type="text"
+                    id="companyInput"
+                    required
+                    >
+                </div>
+            </div>
+            <!-- 카테고리 대분류, 소분류 드롭다운 버튼?-->
+            <!-- 어떻게 구현해야하는지,, -->
+            <div>
+                <label for="category">카테고리</label>
+                <div>
+                    <input
+                    type="text"
+                    id="category_big"
+                    required
+                    >
+                </div>
+                <div>
+                    <input
+                    type="text"
+                    id="category_small"
+                    required
+                    >
+                </div>
+            </div>
+            <div>
+                <label for="stockInput">재고</label>
+                <div>
+                    <input
+                    type="number"
+                    id="stockInput"
+                    required
+                    >
+                </div>
+            </div>
+            <!-- 이미지를 받아와서 삽입해야함. img의 src가 변경되어야합니다! -->
+            <div>
+                <img src="#" class="image-box" />
+                <label for="file" class="upload-btn">
+                <input id="file" type="file" accept="image/*" />
+                </label>
+            </div>
+            </div>
+
+            <div>
+                <label for="descriptionInput">상품상세</label>
+                <div>
+                    <input
+                    type="text"
+                    id="descriptionInput"
+                    required
+                    >
+                </div>
+            </div>
+            <div id="editBtn"><button type="submit">등록</div>
+        </form>
+        </div>`;
+  
+    return field;
+  }
