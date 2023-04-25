2000f9787b1a2bb720c6581fe24bf12f9a30363b
kyounghye <kyoung00828@gmail.com>
2023-04-25 15:34:57 +0900

    fix(logInPage):오타 수정 오타 수정, 로그인 요청 URL 추가

--- src/logInPage/index.js
+++ src/logInPage/index.js
@@ -1,16 +1,16 @@
 import { checkEmail } from "../utils/index.js"
 
-const emailInput = document.querySelector("#emailInput");
-const passwordInput = document.querySelector("#passwordInput");
-const loginBtn = document.querySelector("#loginBtn");
-const nonmemberBtn = document.querySelector("#nonmemberBtn");
+const emailInput = document.querySelector("#emailInput")
+const passwordInput = document.querySelector("#passwordInput")
+const loginBtn = document.querySelector("#loginBtn")
+const nonmemberBtn = document.querySelector("#nonmemberBtn")
 
 // 로그인버튼 이벤트
-loginBtn.addEventListener("click", loginSubmit); 
+loginBtn.addEventListener("click", loginSubmit)
 
 async function loginSubmit(e) {
     // preventDefault 안 하면 새로고침됨.
-    e.preventDefault
+    e.preventDefault()
   
     // 입력값 가져오기
     const email = emailInput.value;
@@ -26,7 +26,7 @@ async function loginSubmit(e) {
     if (!email) {
       return alert("이메일을 입력해 주세요.")
     } else {
-      checkEmail()
+      //비밀번호 체크
     }
     
     if (!password) {
@@ -35,8 +35,8 @@ async function loginSubmit(e) {
     
     // JSON 만들기
     const dataJson = JSON.stringify(data)
-    
-    const apiUrl = `/api/users/login` //수정 필요할지도
+    //로그인 요청 URL
+    const apiUrl = `http://localhost:5000/api/users/login`
   
     const res = await fetch(apiUrl, {
       method: 'POST',
@@ -46,16 +46,17 @@ async function loginSubmit(e) {
       body: dataJson,
     });
     
-    // if (res.status === 200) {
-    //   alert("로그인에 성공하였습니다!")
-    //   window.location.href = '/landingPage';
-    // } else {
-    //   alert("로그인에 실패하였습니다...")
-    //   window.location.href = '/loginPage';
-    // }
+    if (res.status === loginSuccess) {
+      alert("로그인에 성공하였습니다!")
+      window.location.href = '/index.html';
+    } else {
+      alert("로그인에 실패하였습니다...")
+      window.location.href = '/loginPage.html';
+    }
   }
 
   //비회원 주문조회 이벤트
   nonmemberBtn.addEventListener("click", ()=>{
-    window.location.href = '/adminMypage';
+    //비회원 주문 조회 페이지로 이동
+    window.location.href = '/';
   });
