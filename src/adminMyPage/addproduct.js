cfb5172ffff5a37e875aa32e73d5afc1d9f68f27
kyounghye <kyoung00828@gmail.com>
2023-04-28 05:43:27 +0900

    feat(addProduct):상품 FormData로 추가하는 기능 구현중 작성된 내용을 formdata에 담았습니다. 전송할때 오류가 발생하고 있습니다...

--- src/adminMyPage/addproduct.js
+++ src/adminMyPage/addproduct.js
@@ -27,11 +27,13 @@ const categoryInput = document.querySelector("#categoryInput")
 
 const fileUploadBtn = document.querySelector("#fileUploadBtn")
 
+
 //이미지 프리뷰 기능
 fileDOM.addEventListener('change', () => {
   const imageSrc = URL.createObjectURL(fileDOM.files[0])
   preview.src = imageSrc
 });
+
 
 //file 저장
 fileUploadBtn.addEventListener("click", () => {
@@ -39,51 +41,51 @@ fileUploadBtn.addEventListener("click", () => {
   // form Data 객체 생성
 
   formData.append("attachedImage", fileInput.files[0])
-  console.log(fileInput.files[0])
 })
 
 //취소버튼 -> 관리자 페이지로 이동
 cancelBtn.addEventListener("click", () => {window.location.href = "./adminMyPage";})
 saveProductBtn.addEventListener("click", addProductSubmit)
 
+//상품 추가 함수
 async function addProductSubmit(e) {
     e.preventDefault()
     
     const name = nameInput.value
-    const price = priceInput.value
-    const summary = summaryInput.value
-    const company = companyInput.value
-    const category = categoryInput.value
-    const stock = stockInput.value
-    const imagePath = fileInput.name
-    const description = descriptionInput.value
+    // const price = priceInput.value
+    // const summary = summaryInput.value
+    // const company = companyInput.value
+    // const category = categoryInput.value
+    // const stock = stockInput.value
+    // const imagePath = fileInput.name
+    // const description = descriptionInput.value
 
-    const productdata = {
-        name,
-        price,
-        category,
-        description,
-        summary,
-        company,
-        stock,
-        imagePath,
-    }
 
-    console.log(productdata)
-    console.log(JSON.stringify(productdata))
+    // const productdata = {
+    //     name,
+    //     price,
+    //     category,
+    //     description,
+    //     summary,
+    //     company,
+    //     stock,
+    //     imagePath,
+    // }
 
-    
-    if(isAdmin){
-      const dataJson = JSON.stringify(productdata)
+    const formData = new FormData(document.querySelector("#productForm"))
+    console.log(formData)
+    const addData = JSON.stringify(formData)
+
+    //if(isAdmin){
       
       const apiUrl = `http://localhost:5000/api/products/add`
     
       const res = await fetch(apiUrl, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: dataJson,
+        // headers: {
+        //     'Content-Type': 'application/json',
+        // },
+        body:  addData  ,
       });
       
       if (res.ok) {
@@ -91,6 +93,6 @@ async function addProductSubmit(e) {
         } else {
           alert(`상품 등록에 실패하였습니다...`)
         }
-    } 
+    //} 
   }
 
