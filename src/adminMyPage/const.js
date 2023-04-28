9084d474a8e1708df63e7c99f84def2828602cad
Rki0 <pky00823@gmail.com>
2023-04-28 11:45:19 +0900

    feat(all): API 연결 및 변경 사항 반영

--- src/adminMyPage/const.js
+++
@@ -1,1 +0,0 @@
-export const CLICKED_MAJOR_CATEGORY = "텐트";
--- src/cartPage/index.js
+++ src/cartPage/index.js
@@ -4,65 +4,28 @@ import { checkAuth } from "../utils/index.js";
 
 const { isLoggedIn, token } = checkAuth();
 
-let cartData = [
-  {
-    id: 1,
-    src: "https://bellenuit.kr/web/product/medium/202210/9c781c64ee97abde8109dc1c860c4076.jpg",
-    number: 4,
-    name: "쫄깃쫄깃 텐트",
-    company: "모닥불 컴퍼니",
-    price: 1000000,
-    summary: "요약입니다~~",
-  },
-  {
-    id: 2,
-    src: "https://bellenuit.kr/web/product/medium/202210/9c781c64ee97abde8109dc1c860c4076.jpg",
-    number: 1,
-    name: "통통한 모닥불",
-    company: "통통 컴퍼니",
-    price: 30000,
-    summary: "요약입니다~~",
-  },
-  {
-    id: 3,
-    src: "https://bellenuit.kr/web/product/medium/202210/9c781c64ee97abde8109dc1c860c4076.jpg",
-    number: 2,
-    name: "한국 모기장",
-    company: "한국 컴퍼니",
-    price: 1000,
-    summary: "요약입니다~~",
-  },
-  {
-    id: 4,
-    src: "https://bellenuit.kr/web/product/medium/202210/9c781c64ee97abde8109dc1c860c4076.jpg",
-    number: 3,
-    name: "야호 야호",
-    company: "야호 컴퍼니",
-    price: 124000,
-    summary: "요약입니다~~",
-  },
-];
+let cartData = [];
 
 /**
  * 로그인한 유저의 장바구니 데이터를 얻기 위한 API 통신 함수
  */
 async function getCartData() {
-  // 장바구니 데이터 API 통신
-  const response = await fetch("http://localhost:5000/api/carts/view", {
-    method: "GET",
-    headers: {
-      Authorization: "bearer " + token,
-    },
-  });
+  try {
+    // 장바구니 데이터 API 통신
+    const response = await fetch("http://localhost:5000/api/carts/view", {
+      method: "GET",
+      headers: {
+        Authorization: "bearer " + token,
+      },
+    });
 
-  cartData = await response.json();
-  console.log(cartData);
+    const responseData = await response.json();
+
+    cartData = responseData.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-// 생각해보니까 비회원 유저도 장바구니를 쓸 수 있잖아?
-// localStorage에 담겨져 있는 장바구니에 대해서 프론트 팀과 논의 필요
-// renderCarts()에서 로그인/비로그인 분기 처리 필요
-// 로그인은 fetch로 데이터 가져오고, 비로그인은 localStorage에서 데이터 가져오고
 
 /**
  * 로그인 유저의 장바구니 데이터를 받아와서 렌더링하는 함수
@@ -75,9 +38,20 @@ async function renderCarts() {
 
   // 회원/비회원 분기 처리를 통한 장바구니 데이터 할당
   if (isLoggedIn) {
-    getCartData();
+    await getCartData();
   } else {
     cartData = localStorage.getItem("cartData");
+  }
+
+  // 장바구니 데이터가 없는 경우
+  // 비회원의 경우 null, 회원의 경우 []가 된다.
+  if (cartData === null || cartData.length < 1) {
+    const h2 = document.createElement("h2");
+    h2.innerText = "장바구니가 비었습니다.";
+
+    cartsDiv.appendChild(h2);
+
+    return;
   }
 
   for (let i = 0; i < cartData.length; i++) {
@@ -94,10 +68,10 @@ async function renderCarts() {
 
     const productDiv = document.createElement("div");
     productDiv.classList.add("d-flex", "w-75", "align-items-center");
-
+    console.log(cartData);
     const img = document.createElement("img");
-    img.src = `${cartData[i].src}`;
-    img.alt = `${cartData[i].summary}`;
+    img.src = `http://localhost:5000/${cartData[i].imgURL}`;
+    img.alt = "상품 이미지";
     img.classList.add("rounded", "w-25", "pe-2");
 
     const ul = document.createElement("ul");
@@ -112,7 +86,7 @@ async function renderCarts() {
         input.type = "number";
         input.max = "10";
         input.min = "1";
-        input.value = `${cartData[i].number}`;
+        input.value = `${cartData[i].quantity}`;
         input.classList.add("number_input");
 
         // input 변경이 있을 때마다 총 금액 계산 실행
@@ -149,13 +123,10 @@ async function renderCarts() {
     );
 
     // 제거 버튼 클릭 시 API 통신 진행
-    button.addEventListener("click", () => cartDeleteHandler(cartData[i].id));
+    button.addEventListener("click", () => cartDeleteHandler(cartData[i]._id));
 
     buttonDiv.appendChild(button);
 
-    // cartDiv.appendChild(img);
-    // cartDiv.appendChild(ul);
-    // cartDiv.appendChild(button);
     cartDiv.appendChild(buttonDiv);
     cartsDiv.appendChild(cartDiv);
   }
@@ -170,21 +141,30 @@ async function renderCarts() {
 async function cartDeleteHandler(id) {
   // 회원의 경우
   if (isLoggedIn) {
-    // 장바구니 삭제 API 통신
-    // 삭제할 제품의 id를 body로 보낸다.
-    // await fetch("url", {
-    //   method: "DELETE",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: {
-    //     id,
-    //   },
-    // });
+    try {
+      // 장바구니 삭제 API 통신
+      const response = await fetch(
+        `http://localhost:5000/api/carts/deleteOne/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "bearer " + token,
+          },
+        }
+      );
 
-    // 화면에서 지우기
-    // API 통신 성공 시 삭제 / 실패 시 alert 후 return
-    cartData = cartData.filter((item) => item.id !== id);
+      if (!response.ok) {
+        return alert(
+          "장바구니 삭제 중 에러가 발생하였습니다. 다시 시도해주세요."
+        );
+      }
+
+      // 화면에서 지우기
+      cartData = cartData.filter((item) => item.id !== id);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   // 비회원의 경우
@@ -233,6 +213,10 @@ orderButton.addEventListener("click", orderHandler);
  * 구매 진행에 필요한 사항을 객체에 담아 주문 진행 페이지로 전달하는 함수
  */
 function orderHandler() {
+  if (cartData.length < 1) {
+    return alert("장바구니에 아무것도 없어요!");
+  }
+
   // 하나의 원소는 이름, 수량, 총 가격, 회사 데이터를 포함한다.
   let orderData = {
     data: [],
@@ -245,11 +229,11 @@ function orderHandler() {
     orderData.data = [
       ...orderData.data,
       {
+        _id: item._id,
         name: item.name,
-        number: parseInt(input[index].value),
+        quantity: parseInt(input[index].value),
         company: item.company,
-        img: item.src,
-        summary: item.summary,
+        imgURL: item.imgURL,
       },
     ];
   });
