3939337158bf599b7a55ca063b4912b6b7db2770
Rki0 <pky00823@gmail.com>
2023-04-28 16:58:20 +0900

    fix(loginPage): 로그인 시 이동 경로 수정
    랜딩 페이지가 index.html에서 productListPage.html로 변경되었으므로,
    이를 반영하였습니다.

--- src/logInPage/index.js
+++ src/logInPage/index.js
@@ -1,41 +1,41 @@
 import { checkEmail } from "../utils/index.js";
 
-const emailInput = document.querySelector("#emailInput")
-const passwordInput = document.querySelector("#passwordInput")
-const loginBtn = document.querySelector("#loginBtn")
-const nonmemberBtn = document.querySelector("#nonmemberBtn")
-const adminlogInBtn = document.querySelector("#adminlogInBtn")
+const emailInput = document.querySelector("#emailInput");
+const passwordInput = document.querySelector("#passwordInput");
+const loginBtn = document.querySelector("#loginBtn");
+const nonmemberBtn = document.querySelector("#nonmemberBtn");
+const adminlogInBtn = document.querySelector("#adminlogInBtn");
 
 // 로그인버튼 이벤트
-loginBtn.addEventListener("click", loginSubmit)
+loginBtn.addEventListener("click", loginSubmit);
 
 async function loginSubmit(e) {
   // preventDefault 안 하면 새로고침됨.
-  e.preventDefault()
+  e.preventDefault();
 
   // 입력값 가져오기
-  const email = emailInput.value
-  const password = passwordInput.value
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
   // 객체 만듦
   const data = {
     email,
     password,
-  }
+  };
 
   //입력 여부 확인
   if (!email) {
-    return alert("이메일을 입력해 주세요.")
+    return alert("이메일을 입력해 주세요.");
   }
 
   if (!password) {
-    return alert("비밀번호를 입력해 주세요.")
+    return alert("비밀번호를 입력해 주세요.");
   }
 
   // JSON 만들기
-  const dataJson = JSON.stringify(data)
+  const dataJson = JSON.stringify(data);
 
-  const apiUrl = `http://localhost:5000/api/users/login`
+  const apiUrl = `http://localhost:5000/api/users/login`;
 
   const response = await fetch(apiUrl, {
     method: "POST",
@@ -43,7 +43,7 @@ async function loginSubmit(e) {
       "Content-Type": "application/json",
     },
     body: dataJson,
-  })
+  });
 
   const responseData = await response.json();
 
@@ -61,16 +61,16 @@ async function loginSubmit(e) {
 
   if (response.ok) {
     alert("로그인에 성공하였습니다!");
-    window.location.href = "/index.html"
+    window.location.href = "/productListPage.html";
   } else {
-    alert("로그인에 실패하였습니다...")
-    window.location.href = "/logInPage.html"
+    alert("로그인에 실패하였습니다...");
+    window.location.href = "/logInPage.html";
   }
 }
 
 //비회원 주문조회 이벤트
 nonmemberBtn.addEventListener("click", () => {
-  window.location.href = "guestOrderPage.html"
+  window.location.href = "guestOrderPage.html";
 });
 
 // // 관리자 로그인 페이지 전환 이벤트
